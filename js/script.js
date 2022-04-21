e834beb4c2b91d465746830741f609455b1dc67b
MrBrut <80398743+MrBrut-glitch@users.noreply.github.com>
2022-04-21 22:40:44 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,49 @@
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minsEl = document.getElementById("mins");
+const secondsEl = document.getElementById("seconds");
+
+const newYears = "31 Dec 2022";
+
+function countdown() {
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+
+    const totalSeconds = (newYearsDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const mins = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(mins);
+    secondsEl.innerHTML = formatTime(seconds);
+}
+
+setInterval(() => {
+    const colors = [
+        "#2196F3",
+        "#32c787",
+        "#00BCD4",
+        "#ff5652",
+        "#ffc107",
+        "#ff85af",
+        "#FF9800",
+        "#39bbb0"
+    ];
+
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    document.body.style.color = randomColor;
+}, 1000);
+
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+// initial call
+countdown();
+
+setInterval(countdown, 1000);
