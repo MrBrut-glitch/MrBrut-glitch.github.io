e7d2278cb603a57cc70a0a7d00b49e6edcc8a6f6
MrBrut <mrbrut.mb@gmail.com>
2022-05-21 15:15:39 +0200

    Update. UPDATE!

--- js/script.js
+++ js/script.js
@@ -1,56 +1,33 @@
-const daysEl = document.getElementById("days");
-const hoursEl = document.getElementById("hours");
-const minsEl = document.getElementById("mins");
-const secondsEl = document.getElementById("seconds");
-
 const dark_mode_button = document.getElementById("dark_mode_button");
 dark_mode_button.addEventListener("click", () => {
     document.body.classList.toggle("dark_mode");
 });
 
-const newYears = "31 Dec 2022";
+window.onscroll = function() {scrollFunction()};
 
-function countdown() {
-    const newYearsDate = new Date(newYears);
-    const currentDate = new Date();
-
-    const totalSeconds = (newYearsDate - currentDate) / 1000;
-
-    const days = Math.floor(totalSeconds / 3600 / 24);
-    const hours = Math.floor(totalSeconds / 3600) % 24;
-    const mins = Math.floor(totalSeconds / 60) % 60;
-    const seconds = Math.floor(totalSeconds) % 60;
-
-    daysEl.innerHTML = days;
-    hoursEl.innerHTML = formatTime(hours);
-    minsEl.innerHTML = formatTime(mins);
-    secondsEl.innerHTML = formatTime(seconds);
+function scrollFunction() {
+  if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
+    document.getElementById("nav").style.fontSize = "20px";
+    document.getElementById("nav").style.padding = "15px 20px";
+  } else {
+    document.getElementById("nav").style.fontSize = "0px";
+    document.getElementById("nav").style.padding = "0";
+  }
 }
-
-// Make Random Color for a word
 
 setInterval(() => {
     const colors = [
-        "#2196F3",
-        "#32c787",
-        "#00BCD4",
-        "#ff5652",
-        "#ffc107",
-        "#ff85af",
-        "#FF9800",
-        "#39bbb0"
+        "#F6EB0C", // yellow
+        "#F6890C", // yellow
+        "#00BCD4", // Blue
+        "#6FF60F", // Green
+        "#3CD40B", // Green
+        "#F6B02A", // yellow
+        "#FFFFFF"  // White
     ];
 
     const randomColor = colors[Math.floor(Math.random() * colors.length)];
     document.body.style.color = randomColor;
 }, 1000);
-
-
-function formatTime(time) {
-    return time < 10 ? `0${time}` : time;
-}
-
-// initial call
-countdown();
 
 setInterval(countdown, 1000);
