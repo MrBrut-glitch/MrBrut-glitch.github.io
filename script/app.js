91ad2c135d96b21addb7d93942f588b228630f1b
Mrbrut-glitch <mrbrut.mb@gmail.com>
2023-11-19 00:48:49 +0100

    Commit Changes

--- script/app.js
+++ script/app.js
@@ -58,6 +58,7 @@ const loadHeroAnimation = () => {
       onComplete: function() {
         startTextAnimation()
         setActiveSection()
+        cursor.classList.remove("hidden")
       }
     }
   );
